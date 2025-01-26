dc6be6b73137f841987ce87dd0dfa9a590337e7d
goldmeninalmaty <146926829+Ulyk04@users.noreply.github.com>
2025-01-26 21:53:55 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  const sections = document.querySelectorAll("section") ;
+
+  const observerOptions = {
+    threshold: 0.2, 
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const sectionElements = entry.target.querySelectorAll("article, div, img, button");
+        sectionElements.forEach((el) => {
+          el.classList.add("visible");
+        });
+        observer.unobserve(entry.target); 
+      }
+    });
+  }, observerOptions);
+
+  sections.forEach((section) => observer.observe(section));
+});
